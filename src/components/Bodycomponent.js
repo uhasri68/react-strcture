27ec4a2c555c00cd9cf07a3b771e71efb7e2c122
uhasri68 <uhasri.tumu@gmail.com>
2024-05-17 15:55:27 +0530

    created top rated button and added functionality

--- src/components/Bodycomponent.js
+++ src/components/Bodycomponent.js
@@ -1,33 +1,62 @@
 import RestutantCard from "./RestutantCard";
 import resList from "../utilties/Mockdata";
+import {useState} from "react";
  //not using keys (not acceptable) <<<<< index as key <<<<<<<<<<<<<<<Unique Id
-  const Bodycomponent = () =>{
+  let Bodycomponent = () =>{
+
+    //local state Varible in React for that we used hooks
+let  [listOfResturants  , setListOfResturants ] = useState(resList)
+//   {
+//     "info": {
+//           "id": "11091",
+//           "name": "Pizza Hut",
+//           "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+//           "areaName": "Rambagh Colony",
+//           "costForTwo": "₹300 for two",   
+//              "avgRating": 4.1,
+         
+//         },}, { 
+//            "info": {
+//           "id": "11092",
+//           "name": "Kritunga",
+//           "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+//           "areaName": "Rambagh Colony",
+//           "costForTwo": "₹300 for two",   
+//              "avgRating": 4.3,
+         
+//         },},{
+//           "info": {
+//                   "id": "11095",
+//                   "name": "KFC",
+//                   "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+//                   "areaName": "Rambagh Colony",
+//                   "costForTwo": "₹300 for two",   
+//                      "avgRating": 3.8,
+                 
+//                 },
+// }
+      
+// ]);
+
     return (
        <div className="body">
         <div className="filter">
-          {/* onClick will take the cllback function */}
           <button className="filter-btn" 
-          onClickon={() =>
+          onClick={() =>
+         // onClick will take the cllback function  
             {console.log("button clicked")
+            //filter logic here
+            const filterList =listOfResturants.filter(res =>res.info.avgRating > 4
 
+            );
+           setListOfResturants(filterList)
             }} 
             >
               Top Rated Restutants</button>
         </div>
 
         <div className="res-container">
-
-      {/* <RestutantCard resData ={resList[0]}/>
-      <RestutantCard resData ={resList[1]}/>
-      <RestutantCard resData ={resList[2]}/>
-      <RestutantCard resData ={resList[3]}/>
-      <RestutantCard resData ={resList[4]}/>
-      <RestutantCard resData ={resList[5]}/>
-      <RestutantCard resData ={resList[6]}/>
-      <RestutantCard resData ={resList[7]}/>
-      <RestutantCard resData ={resList[8]}/>
-      <RestutantCard resData ={resList[0]}/> */}
-    {resList.map((restaurant) =>(
+    {listOfResturants.map((restaurant) =>(
       <RestutantCard key ={restaurant.info.id} resData ={restaurant}/>
     )
     )}  
