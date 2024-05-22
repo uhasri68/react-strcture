3b8e4526bc3d2f7b3ba7611bf1e65b45da201bfa
uhasri68 <uhasri.tumu@gmail.com>
2024-05-22 10:59:27 +0530

    filtering resturant info

--- src/components/Bodycomponent.js
+++ src/components/Bodycomponent.js
@@ -1,50 +1,35 @@
 import RestutantCard from "./RestutantCard";
 import resList from "../utilties/Mockdata";
-import {useState} from "react";
+import {useEffect, useState} from "react";
  //not using keys (not acceptable) <<<<< index as key <<<<<<<<<<<<<<<Unique Id
   let Bodycomponent = () =>{
 
     //local state Varible in React for that we used hooks
-let  [listOfResturants  , setListOfResturants ] = useState(resList)
-//   {
-//     "info": {
-//           "id": "11091",
-//           "name": "Pizza Hut",
-//           "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
-//           "areaName": "Rambagh Colony",
-//           "costForTwo": "₹300 for two",   
-//              "avgRating": 4.1,
-         
-//         },}, { 
-//            "info": {
-//           "id": "11092",
-//           "name": "Kritunga",
-//           "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
-//           "areaName": "Rambagh Colony",
-//           "costForTwo": "₹300 for two",   
-//              "avgRating": 4.3,
-         
-//         },},{
-//           "info": {
-//                   "id": "11095",
-//                   "name": "KFC",
-//                   "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
-//                   "areaName": "Rambagh Colony",
-//                   "costForTwo": "₹300 for two",   
-//                      "avgRating": 3.8,
-                 
-//                 },
-// }
-      
-// ]);
+let  [listOfResturants  , setListOfResturants ] = useState(resList);
+useEffect(() =>{
+fetchData ();
+} , [])
+const fetchData = async() => {
+  const data =await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    
+  );
+  const json = await data.json();
+  console.log(
+    json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+  )
+  
+}
+
 
     return (
        <div className="body">
         <div className="filter">
           <button className="filter-btn" 
-          onClick={() =>
+          onClick={
+            () =>
          // onClick will take the cllback function  
-            {console.log("button clicked")
+            {
+              fetchData();
             //filter logic here
             const filterList =listOfResturants.filter(res =>res.info.avgRating > 4
 
