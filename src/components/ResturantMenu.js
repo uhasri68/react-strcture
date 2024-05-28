5c421cb2e2946b6f37ed3354f255c70fb04d4992
uhasri68 <uhasri.tumu@gmail.com>
2024-05-28 17:12:04 +0530

    making api call

--- src/components/ResturantMenu.js
+++ src/components/ResturantMenu.js
@@ -1,6 +1,19 @@
 import React from "react";
+import { useEffect } from "react";
 
 const ResturantMenu = () => {
+useEffect(() =>{
+fetchMenu();
+
+}, [])
+const fetchMenu = async() =>{
+  const resMenu =  await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.37240&lng=78.43780&restaurantId=659668&catalog_qa=undefined&submitAction=ENTER");
+  const json = await resMenu.json();
+  console.log(json);
+
+}
+
+
   return(
     <div className="menu">
         <h3>Name of the Resturant </h3> 
