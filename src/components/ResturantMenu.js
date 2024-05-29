9b5cf50eeca5442132ad49ba239a9e6251580bde
uhasri68 <uhasri.tumu@gmail.com>
2024-05-29 16:35:52 +0530

    created dynamic routing using api call

--- src/components/ResturantMenu.js
+++ src/components/ResturantMenu.js
@@ -1,27 +1,38 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect , useState} from "react";
+import Shimmer from "./Shimmer";
 
 const ResturantMenu = () => {
-useEffect(() =>{
-fetchMenu();
+  const [resinfo , setResInfo] =useState(null)
 
-}, [])
-const fetchMenu = async() =>{
-  const resMenu =  await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.37240&lng=78.43780&restaurantId=659668&catalog_qa=undefined&submitAction=ENTER");
-  const json = await resMenu.json();
-  console.log(json);
+  useEffect(() =>{
+    fetchMenu();
+  }, [])
 
-}
+  const fetchMenu = async() =>{
+    const resMenu =  await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.37240&lng=78.43780&restaurantId=659668&catalog_qa=undefined&submitAction=ENTER");
+    const json = await resMenu.json();
+    console.log(json);
+    setResInfo(json);
+   }
+ 
+  if(resinfo === null) return <Shimmer/>
 
+  // destructure here after the loading check
+  const {name,
+      city,
+      cuisines,
+       costForTwoMessage,
+        avgRating} = resinfo.data.cards[2].card.card.info;
 
   return(
     <div className="menu">
-        <h3>Name of the Resturant </h3> 
-        <h3>Menu</h3>
+        <h3>{name}</h3> 
+        <h3>{city}</h3>
         <ul>
-            <li>Biryani</li>
-            <li>Chinese</li>
-            <li>Italian Food</li>
+            <li>{cuisines}</li>
+            <li>{costForTwoMessage}</li>
+            <li>{avgRating}</li>
         </ul>
     </div>
   )
