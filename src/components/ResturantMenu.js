7693066e811996724c9926b905279aa2675aa13a
uhasri68 <uhasri.tumu@gmail.com>
2024-06-03 17:29:56 +0530

    ui changes

--- src/components/ResturantMenu.js
+++ src/components/ResturantMenu.js
@@ -1,16 +1,59 @@
+// import React from "react";
+// import { useEffect , useState} from "react";
+// import Shimmer from "./Shimmer";
+// import { useParams } from "react-router-dom";
+// const ResturantMenu = () => {
+//   const [resinfo , setResInfo] =useState(null)
+// const {resId} = useParams()
+//   useEffect(() =>{
+//     fetchMenu();
+//   }, [])
+
+//   const fetchMenu = async() =>{
+//     const resMenu =  await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.37240&lng=78.43780&restaurantId=${`resId`}&catalog_qa=undefined&submitAction=ENTER");
+//     const json = await resMenu.json();
+//     console.log(json);
+//     setResInfo(json);
+//    }
+ 
+//   if(resinfo === null) return <Shimmer/>
+
+//   // destructure here after the loading check
+//   const {name,
+//       city,
+//       cuisines,
+//        costForTwoMessage,
+//         avgRating} = resinfo.data.cards[2].card.card.info;
+
+//   return(
+//     <div className="menu">
+//         <h3>{name}</h3> 
+//         <h3>{city}</h3>
+//         <ul>
+//             <li>{cuisines}</li>
+//             <li>{costForTwoMessage}</li>
+//             <li>{avgRating}</li>
+//         </ul>
+//     </div>
+//   )
+// }
+
+// export default ResturantMenu;
+
 import React from "react";
 import { useEffect , useState} from "react";
 import Shimmer from "./Shimmer";
-
+import { useParams } from "react-router-dom";
 const ResturantMenu = () => {
   const [resinfo , setResInfo] =useState(null)
+  const {resId} = useParams()
 
   useEffect(() =>{
     fetchMenu();
-  }, [])
+  }, [resId])
 
   const fetchMenu = async() =>{
-    const resMenu =  await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.37240&lng=78.43780&restaurantId=659668&catalog_qa=undefined&submitAction=ENTER");
+    const resMenu =  await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.37240&lng=78.43780&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`);
     const json = await resMenu.json();
     console.log(json);
     setResInfo(json);
@@ -23,10 +66,10 @@ const ResturantMenu = () => {
       city,
       cuisines,
        costForTwoMessage,
-        avgRating} = resinfo.data.cards[2].card.card.info;
+        avgRating} = resinfo?.data?.cards[2]?.card?.card?.info;
 
   return(
-    <div className="menu">
+    <div className="menu"> 
         <h3>{name}</h3> 
         <h3>{city}</h3>
         <ul>
--- src/utilties/constants.js
+++ src/utilties/constants.js
@@ -1,3 +1,5 @@
 export const CDN_URL  = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const LOGO_URL =  "https://cdn3.vectorstock.com/i/1000x1000/24/12/food-chat-app-logo-design-vector-34122412.jpg";
+
+// export  const MENU_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.37240&lng=78.43780&restaurantId"
